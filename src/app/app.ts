138d6fac8626c04a2e456b7202a8a0a9f744eccb
Yannick Van kerckvoorde <vankerckvoordeyannick@gmail.com>
2025-08-25 15:53:37 +0200

    Basic Login Form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoginForm} from './login/LoginForm';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/login/LoginForm.ts
@@ -0,0 +1,11 @@
+﻿import {Component, signal} from '@angular/core';
+
+@Component({
+  selector: 'login-form',
+  imports: [],
+  templateUrl: './loginForm.html',
+  styleUrl: './Form.css'
+})
+export class LoginForm {
+
+}
