9e8f35a1565db632f08b02ff5b31804957587e47
Yannick Van kerckvoorde <vankerckvoordeyannick@gmail.com>
2025-08-26 08:56:37 +0200

    Added simple databind

--- src/app/login/LoginForm.ts
+++ src/app/login/LoginForm.ts
@@ -1,11 +1,15 @@
 ﻿import {Component, signal} from '@angular/core';
+import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'login-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './loginForm.html',
   styleUrl: './Form.css'
 })
 export class LoginForm {
-
+  username = new FormControl('');
+  password = new FormControl('');
 }
