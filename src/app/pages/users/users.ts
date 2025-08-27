c302b95e55cc97f4a33221274013321b90856966
Yannick <vankerckvoordeyannick@gmail.com>
2025-08-27 15:57:57 +0200

    Get all users from api and put them into a table

--- src/app/pages/users/users.ts
+++ src/app/pages/users/users.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal, WritableSignal} from '@angular/core';
+import {UserService} from '../../services/user-service';
+import {User} from '../../models/Users';
+import {Observable} from 'rxjs';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './users.html',
   styleUrl: './users.scss'
 })
 export class Users {
+  userService = inject(UserService);
+  users: WritableSignal<Array<User>> = signal([]);
 
+  constructor() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    console.log('Is logged in:', this.userService.authService.IsLoggedIn());
+    this.userService.getUsers().subscribe({
+      next: (users) => {
+        this.users.set(users);
+      },
+      error: (err) => {
+        console.error('Error getting users:', err);
+      }
+    });
+  }
 }
--- src/app/services/user-service.ts
+++ src/app/services/user-service.ts
@@ -1,7 +1,8 @@
 import {inject, Injectable} from '@angular/core';
 import {AuthService} from './AuthService';
 import {User} from '../models/Users';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {catchError, of, tap} from 'rxjs';
 
 @Injectable({
   'providedIn': 'root'
@@ -11,9 +12,22 @@ export class UserService {
   private client = inject(HttpClient)
   private baseUrl = 'https://localhost:7134/api/v1/Users';
 
-  public getUsers(): Array<User> {
-    if (!this.authService.IsLoggedIn()) { return []; }
+  public getUsers() {
+    if (!this.authService.IsLoggedIn()) {
+      console.error('Users not logged in');
+      return of([]);
+    }
 
-    return [];
+    return this.client.get<Array<User>>(`${this.baseUrl}`)
+      .pipe(
+        tap(authResult => {
+          console.table(authResult);
+          return authResult;
+        }),
+        catchError((error: HttpResponse<any>) => {
+          console.error('Failed to get users: ', error);
+          throw error;
+        }));
   }
 }
+
