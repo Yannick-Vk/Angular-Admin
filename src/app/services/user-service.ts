b5109dc67560a85761e5d8c657069978c2e1b9df
Yannick <vankerckvoordeyannick@gmail.com>
2025-08-27 15:19:30 +0200

    Added baseUrl and HttpClient

--- src/app/services/user-service.ts
+++ src/app/services/user-service.ts
@@ -1,12 +1,15 @@
 import {inject, Injectable} from '@angular/core';
 import {AuthService} from './AuthService';
 import {User} from '../models/Users';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   'providedIn': 'root'
 })
 export class UserService {
   authService: AuthService = inject(AuthService);
+  private client = inject(HttpClient)
+  private baseUrl = 'https://localhost:7134/api/v1/Users';
 
   public getUsers(): Array<User> {
     if (!this.authService.IsLoggedIn()) { return []; }
