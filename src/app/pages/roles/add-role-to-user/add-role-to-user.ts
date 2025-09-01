7b2555ad9b47a618e3d34630d98492de04adc7bc
Yannick <vankerckvoordeyannick@gmail.com>
2025-09-01 13:47:08 +0200

    Selects now show the first option

--- src/app/pages/roles/add-role-to-user/add-role-to-user.ts
+++ src/app/pages/roles/add-role-to-user/add-role-to-user.ts
@@ -1,4 +1,4 @@
-import {Component, input, output, ViewChild} from '@angular/core';
+import {Component, computed, input, output, ViewChild} from '@angular/core';
 import {Form} from "../../../components/forms/form/form";
 
 @Component({
@@ -13,6 +13,21 @@ export class AddRoleToUser {
   @ViewChild(Form) formComponent!: Form;
   addRole = output<{RoleName: string, Username: string}>()
   options = input<string[]>()
+
+  formOptions = computed(() => {
+    const opts = this.options();
+    if (!opts) return [];
+    return opts.map(opt => ({ label: opt, value: opt }));
+  })
+
+  formData = computed(() => {
+    const options = this.formOptions();
+    const initialValue = options.length > 0 ? options[0].value : '';
+    return [
+      { label: "Role name", key: "RoleName", type: "select", options: options, required: true, value: initialValue },
+      { label: "Username", key: "Username", type: "text", required: true },
+    ];
+  });
 
   onSubmit(form: {RoleName: string, Username: string}) {
 
