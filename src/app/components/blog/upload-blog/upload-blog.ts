b7310a4f4d917c6fd34dcdfcd5a294e9aa86f7ea
Yannick <vankerckvoordeyannick@gmail.com>
2025-09-01 15:55:14 +0200

    Added upload-blog component

---
+++ src/app/components/blog/upload-blog/upload-blog.ts
@@ -0,0 +1,11 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-blog',
+  templateUrl: './upload-blog.html',
+  styleUrl: './upload-blog.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UploadBlog {
+
+}
