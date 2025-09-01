4c2c8a13a351538f8e11dc13f6b6b3edb4a60b68
Yannick <vankerckvoordeyannick@gmail.com>
2025-09-01 15:59:51 +0200

    Added base form

--- src/app/components/blog/upload-blog/upload-blog.ts
+++ src/app/components/blog/upload-blog/upload-blog.ts
@@ -1,11 +1,31 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {Form} from '../../forms/form/form';
+import {ReactiveFormsModule, ValidationErrors} from '@angular/forms';
 
 @Component({
   selector: 'app-upload-blog',
   templateUrl: './upload-blog.html',
   styleUrl: './upload-blog.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    Form,
+    ReactiveFormsModule
+  ]
 })
 export class UploadBlog {
+  errorMessage: string | undefined;
+  showValidationErrors: boolean = false;
+  fileUpload: string | undefined;
 
+  onFormErrorsChanged($event: ValidationErrors | null) {
+
+  }
+
+  isValid() {
+    return false;
+  }
+
+  onSubmit($event: any) {
+
+  }
 }
