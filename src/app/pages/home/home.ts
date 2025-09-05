890c1f54af165b67597609c39ac4206cf9303473
Yannick <vankerckvoordeyannick@gmail.com>
2025-09-05 09:55:06 +0200

    Updated search to filter local blogs instead of sending an api request

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,9 +2,7 @@
 import {BlogPost} from '../../components/blog/blog-post.component';
 import {BlogService} from '../../services/blog.service';
 import {Blog} from '../../models/Blog';
-import {CopyToClipboard} from '../../services/LinkService';
 import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
-import {Form} from '../../components/forms/form/form';
 
 @Component({
   selector: 'home',
@@ -20,7 +18,10 @@ import {Form} from '../../components/forms/form/form';
 
 export class HomeComponent {
   blogService = inject(BlogService);
+  private allBlogs = signal<Blog[]>([]);
   blogs = signal<Blog[]>([]);
+  errorMessage = signal<string | null>(null)
+
 
   searchForm = new FormGroup({
     searchText: new FormControl('')
@@ -32,20 +33,50 @@ export class HomeComponent {
 
   getBlogPosts(): void {
     this.blogService.getBlogPosts().subscribe((blogPosts) => {
-      // console.table(blogPosts);
+      this.allBlogs.set(blogPosts);
       this.blogs.set(blogPosts);
     })
   }
 
+  // Reset the filter
+  private setUnfiltered() {
+    this.blogs.set(this.allBlogs());
+  }
+
   onSubmit() {
-    const searchText = this.searchForm.controls.searchText.value;
-    if (!searchText || searchText === '') {
-      this.getBlogPosts();
+    this.errorMessage.set(null);
+    let searchText = this.searchForm.controls.searchText.value;
+    const filtered = this.filterBlogs(searchText);
+
+    if (!filtered) {
       return;
     }
 
-    this.blogService.searchBlog(searchText).subscribe((blogPosts) => {
-      this.blogs.set(blogPosts);
-    })
+    if (filtered.length === 0) {
+      this.setUnfiltered();
+      this.errorMessage.set(`No blog's found with title: '${searchText}'`)
+      return;
+    }
+
+    this.blogs.set(filtered);
+  }
+
+  filterBlogs(searchText: string | null): Array<Blog> | null {
+    if (!searchText) {
+      return null;
+    }
+    searchText = searchText.trim().toLowerCase();
+    if (searchText === '') {
+      return null;
+    }
+
+    return this.allBlogs().filter((blog: Blog) =>
+      blog.title.toLowerCase().includes(searchText)
+    );
+  }
+
+  clearSearch() {
+    this.searchForm.controls.searchText.setValue('');
+    this.setUnfiltered();
   }
 }
