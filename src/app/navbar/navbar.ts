4ccfdf39ffb58e804167a6c3e8afd8434a471fc3
Yannick <vankerckvoordeyannick@gmail.com>
2025-09-05 11:36:55 +0200

    Added isAdmin check to navbar

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -2,6 +2,7 @@ import {Component, inject, signal} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {AuthService} from '../services/AuthService';
 import {CommonModule} from "@angular/common";
+import {RoleService} from '../services/role-service';
 
 @Component({
   selector: 'app-navbar',
@@ -15,16 +16,33 @@ import {CommonModule} from "@angular/common";
 })
 export class Navbar {
   private authService = inject(AuthService);
+  private roleService = inject(RoleService);
   loggedIn = signal(this.authService.IsLoggedIn());
+  isAdmin = signal<boolean>(false);
   isHamburgerMenuOpen = signal(false)
 
   constructor() {
     this.authService.isLoggedIn$.subscribe(loggedIn => {
       this.loggedIn.set(loggedIn);
+      if (loggedIn) {
+        this.isAdminRole();
+      } else {
+        this.isAdmin.set(false);
+      }
     });
   }
 
   toggleHamburgerMenu() {
     this.isHamburgerMenuOpen.set(!this.isHamburgerMenuOpen());
+  }
+
+  private isAdminRole() {
+    const user = this.authService.getUser();
+    if (!user) return;
+
+    this.roleService.UserIsAdmin(user.username).subscribe(value => {
+        this.isAdmin.set(value);
+      }
+    );
   }
 }
