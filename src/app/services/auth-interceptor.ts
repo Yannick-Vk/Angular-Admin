e92bffd239a0ef3ceed6daccf3e9c40f61255eb5
Yannick <vankerckvoordeyannick@gmail.com>
2025-09-24 09:21:27 +0200

    Updated Intercept to only add token to calls to the Api

--- src/app/services/auth-interceptor.ts
+++ src/app/services/auth-interceptor.ts
@@ -2,17 +2,18 @@ import {HttpEvent, HttpHandlerFn, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {AuthService} from './AuthService';
 import {inject} from '@angular/core';
+import {api_base_url} from './Api';
 
 export function AuthInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<any>> {
     const loggedIn = inject(AuthService).IsLoggedIn();
-    // If the user is not logged in then skip the intercept
-    if (!loggedIn) {
-        return next(req);
+
+    if (loggedIn && req.url.startsWith(api_base_url)) {
+        const cloned = req.clone({
+            withCredentials: true,
+        });
+
+        return next(cloned);
     }
 
-    const cloned = req.clone({
-        withCredentials: true
-    });
-
-    return next(cloned);
+    return next(req);
 }
